404c6f987ad99b701ed5dbb36f864bcdbb0d39e2
Mike <miketu926@gmail.com>
2019-05-20 14:13:19 -0400

    Started main component

---
+++ src/component/main/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+
+const Main = () => {
+
+  return (
+    <div className='main'>
+      <div className='main-header'>main</div>
+      {/* counter */}
+
+
+      {/* salary input */}
+      {/* tax input */}
+
+
+
+    </div>
+  )
+
+};
+
+export default Main;
