0a8a4f99fe1f9ed3f1079094972bd5da99ef3589
Mike <miketu926@gmail.com>
2019-05-20 19:11:34 -0400

    Added Odometer library and created the component

--- src/component/counter/counter.jsx
+++ src/component/counter/counter.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import './style.css';
+import Odometer from "react-odometerjs";
+import './styles.css';
+import './digital.css';
+// import "odometer/themes/odometer-theme-digital.css";
 
 
 const Counter = () => {
@@ -7,24 +10,25 @@ const Counter = () => {
   const [salary, setSalary] = useState(0);
   const [taxRate, setTaxRate] = useState(0);
   const [counterDispStatus, setCounterDispStatus] = useState(false);
+  const [balance, setBalance] = useState(0);
 
   // useEffect(() => {
   // }, []);
 
   const postTaxSalary = (salary, taxRate) => {
-    return (salary - (salary * (taxRate / 100)));
+    return (salary - (salary * (taxRate / 100.00))).toFixed(6);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setBalance(postTaxSalary(salary, taxRate));
     setCounterDispStatus(true);
   };
 
   if (counterDispStatus) {
     return (
-      <div className='counter'>
-        COUNTER starting @ {postTaxSalary(salary, taxRate)}
-      </div>
+      // <Odometer value={balance} duration={500} format="(,ddd).dddd" />
+      <Odometer format="(,ddd).dddddd" duration={500} value={balance} />
     )
   } else {
     return (
@@ -34,8 +38,6 @@ const Counter = () => {
           <input type="number" placeholder="tax %" min="0" max="100" onChange={e => setTaxRate(e.currentTarget.valueAsNumber)} />
           <input type="submit" value="GO" />
         </form>
-
-
       </div>
     )
   }
--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css';
+import './styles.css';
 
 import Counter from '../counter/counter.jsx';
 
