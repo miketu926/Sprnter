ee1d6aadc58a25d8ec5c09bacba7c52fa93dc1f3
Mike <miketu926@gmail.com>
2019-05-25 15:24:42 -0400

    Separated counter component from main display

--- src/component/counter/counter.jsx
+++ src/component/counter/counter.jsx
@@ -1,47 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Odometer from "react-odometerjs";
 import './styles.css';
 import './digital.css';
 // import "odometer/themes/odometer-theme-digital.css";
 
 
-const Counter = () => {
+function Counter({ balance, setBalance }) {
+  const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
 
-  const [salary, setSalary] = useState(0);
-  const [taxRate, setTaxRate] = useState(0);
-  const [counterDispStatus, setCounterDispStatus] = useState(false);
-  const [balance, setBalance] = useState(0);
-
-  // useEffect(() => {
-  // }, []);
-
-  const postTaxSalary = (salary, taxRate) => {
-    return (salary - (salary * (taxRate / 100.00))).toFixed(6);
+    useEffect(() => {
+      const tick = () => { savedCallback.current(); };
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }, [delay]);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setBalance(postTaxSalary(salary, taxRate));
-    setCounterDispStatus(true);
-  };
+  useInterval(() => {
+    setBalance(balance + 1);
+  }, 100);
 
-  if (counterDispStatus) {
-    return (
-      // <Odometer value={balance} duration={500} format="(,ddd).dddd" />
-      <Odometer format="(,ddd).dddddd" duration={500} value={balance} />
-    )
-  } else {
-    return (
-      <div className='counter'>
-        <form onSubmit={handleSubmit}>
-          <input type="number" placeholder="salary" min="0" step="1000" onChange={e => setSalary(e.currentTarget.valueAsNumber)} />
-          <input type="number" placeholder="tax %" min="0" max="100" onChange={e => setTaxRate(e.currentTarget.valueAsNumber)} />
-          <input type="submit" value="GO" />
-        </form>
-      </div>
-    )
-  }
-
+  return (
+    <Odometer className='odometer' format="(,ddd).dddddd" value={balance} />
+  );
 
 };
 
--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Counter from '../counter/counter.jsx';
 import './styles.css';
 
-import Counter from '../counter/counter.jsx';
 
+function Main() {
+  const [salary, setSalary] = useState(0);
+  const [taxRate, setTaxRate] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [displayCounter, setDisplayCounter] = useState(false);
 
-const Main = () => {
+  const handleSubmit = (e) => {
+    // set up currentBalance using new Date determines today's balance since 1/1/YYYY
+    e.preventDefault();
+    let today = new Date();
 
-  return (
-    <div className='main'>
-      <div className='main-header'>main title</div>
+    let currentBalance = (salary - (salary * (taxRate / 100.00))).toFixed(6);
 
-      <Counter />
+    setBalance(currentBalance);
+    setDisplayCounter(true);
+  };
 
-      {/* salary input */}
-      {/* tax input */}
-    </div>
-  )
+  if (displayCounter) {
+    return (
+      <div className='main'>
+        <div className='main-header'>main title</div>
+        <div className='counter'>
+          <Counter
+            balance={balance}
+            setBalance={setBalance}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='main'>
+        <div className='main-header'>main title</div>
+
+        <div className='counter'>
+          <form onSubmit={handleSubmit}>
+            <input type="number" placeholder="salary" min="0" step="1" onChange={e => setSalary(e.target.valueAsNumber)} />
+            <input type="number" placeholder="tax %" min='0' max='100' onChange={e => setTaxRate(e.target.valueAsNumber)} />
+            <input type="submit" value="GO" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+
 
 };
 
